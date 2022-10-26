cf0713ca70371b07aac8ded09d112f59c566d000
Tamirlan4 <tamirlan_agubaev@mail.ru>
2022-10-27 05:19:14 +0600

    27/10/2022/5:19

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,35 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public Animal() {
+
+    }
+
+
+    public String getDescription(){
+        String p;
+        String f;
+        if(numberOfPaws > 1){
+            p = "paws";
+        }else {
+            p = "paw";
+        }
+        if(hasFur == false){
+            f = "no";
+        }else {
+            f = "a";
+        }
+        String text = ("This animal is mostly " + "" + color + "." + " It has " + "" + numberOfPaws + " " + p + " " + "and " + f + " fur.");
+        return text;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,27 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
-
+public class Dog extends Animal{
+    private String color = "brown";
+    private int numberOfPaws = 4;
+    private boolean hasFur = true;
+    public Dog(){
+        super();
+    }
+    @Override
+    public String getDescription() {
+        String p;
+        String f;
+        if(numberOfPaws > 1){
+            p = "paws";
+        }else {
+            p = "paw";
+        }
+        if(hasFur == false){
+            f = "no";
+        }else {
+            f = "a";
+        }
+        String text = ("This animal is mostly " + "" + color + "." + " It has " + "" + numberOfPaws + " " + p + " " + "and " + f + " fur.");
+        return text;
+    }
 }
